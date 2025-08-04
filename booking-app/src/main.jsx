b1f02068b86691e5e1babae70af8d53d461441cb
Kira Mustieva <kira.mustieva@edu.tampere.fi>
2025-08-04 11:42:33 +0300

    vite + build

--- booking-app/src/main.jsx
+++ booking-app/src/main.jsx
@@ -2,8 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const rootEl = document.getElementById("booking-form-root");
+
+if (rootEl) {
+  createRoot(rootEl).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
--- booking-app/vite.config.js
+++ booking-app/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./",
+  build: {
+    outDir: "build",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, "src/main.jsx"),
+      output: {
+        entryFileNames: "assets/index.js",
+        assetFileNames: "assets/index.css",
+      },
+    },
+  },
+});
